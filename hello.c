7cef7b8aba04037fff522bdcd61538efeb4318dc
Victor Chong <victor.chong@linaro.org>
2016-11-11 06:07:40 +0000

    test without build commands and script

--- hello.c
+++ hello.c
@@ -3,17 +3,20 @@
 
 int main(int argc, char **argv)
 {
-	int i;
+	int i, j;
 
 	if(argc == 1)
 	{
 		i=1;
 		printf("hello i = %d\n", i);
+		printf("hello j (undefined) = %d\n", j);
 		return 0;
 	}
 	else
 	{
+		j=1;
 		printf("hello i (undefined) = %d\n", i);
+		printf("hello j = %d\n", j);
 		return 0;
 	}
 }
