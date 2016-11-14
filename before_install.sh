401572f4146e3eb069c6777b6a38a4e67891dd73
Victor Chong <victor.chong@linaro.org>
2016-11-14 06:41:37 +0000

    test COVERITY_SCAN_BRANCH in script

--- before_install.sh
+++ before_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo "Entering before_install.sh"
+if [ "${COVERITY_SCAN_BRANCH}" != 1 ]; then echo "defined" ; else echo "undefined" ; fi
--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "Entering script.sh"
+
+if [ "${COVERITY_SCAN_BRANCH}" != 1 ]; then echo "defined" ; else echo "undefined" ; fi
+
 make clean
 make
 ./hello
